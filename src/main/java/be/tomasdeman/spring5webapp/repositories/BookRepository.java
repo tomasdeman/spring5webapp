c0abb1cdcf64685c47100a4738f7204d563e6a62
tomasdeman <tomas.deman@gmail.com>
2020-01-11 15:59:39 +0100

    added crudrepositories

---
+++ src/main/java/be/tomasdeman/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,8 @@
+package be.tomasdeman.spring5webapp.repositories;
+
+import be.tomasdeman.spring5webapp.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+}
